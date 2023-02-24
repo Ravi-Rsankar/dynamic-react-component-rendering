d7aa111243bd1ee7ae92867ef6401848a4ba31d0
Ravi-Rsankar <ravisankar.r93@gmail.com>
2023-02-24 22:51:07 +0530

    parent child dynamic rendering

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Child() {
-    return <div>Child</div>;
+export default function Child(props) {
+    const [selectedOption1, setSelectedOption1] = useState("");
+    const [selectedOption2, setSelectedOption2] = useState("");
+    const [selectedOption3, setSelectedOption3] = useState("");
+    const [textInput, setTextInput] = useState("");
+
+    const handleOption1Change = (event) => {
+        setSelectedOption1(event.target.value);
+        props.handleSave(props.id, "option1", event.target.value, textInput);
+    };
+
+    const handleOption2Change = (event) => {
+        setSelectedOption2(event.target.value);
+        props.handleSave(props.id, "option2", event.target.value, textInput);
+    };
+
+    const handleOption3Change = (event) => {
+        setSelectedOption3(event.target.value);
+        props.handleSave(props.id, "option3", event.target.value, textInput);
+    };
+
+    const handleTextInputChange = (event) => {
+        setTextInput(event.target.value);
+    };
+
+    return (
+        <div className="radio-group">
+            <div className="radio">
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        checked={selectedOption1 === "option1"}
+                        onChange={handleOption1Change}
+                    />
+                    Option 1
+                </label>
+            </div>
+            <div className="radio">
+                <label>
+                    <input
+                        type="radio"
+                        value="option2"
+                        checked={selectedOption2 === "option2"}
+                        onChange={handleOption2Change}
+                    />
+                    Option 2
+                </label>
+            </div>
+            <div className="radio">
+                <label>
+                    <input
+                        type="radio"
+                        value="option3"
+                        checked={selectedOption3 === "option3"}
+                        onChange={handleOption3Change}
+                    />
+                    Option 3
+                </label>
+            </div>
+            <div className="text-input">
+                <label>
+                    Text Input:
+                    <input type="text" value={textInput} onChange={handleTextInputChange} />
+                </label>
+            </div>
+        </div>
+    );
 }
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import Child from "./Child";
 
 export default function Parent() {
-    return <div>Parent</div>;
+    const [numChildren, setNumChildren] = useState(0);
+    const [radioButtonStates, setRadioButtonStates] = useState({});
+
+    const handleAddChild = () => {
+        setNumChildren(numChildren + 1);
+    };
+
+    const handleSave = (id, name, value, textInputValue) => {
+        setRadioButtonStates((prevState) => {
+            return {
+                ...prevState,
+                [id]: {
+                    option1: prevState[id]?.option1 || "",
+                    option2: prevState[id]?.option2 || "",
+                    option3: prevState[id]?.option3 || "",
+                    textInputValue: textInputValue || "",
+                    [name]: value,
+                },
+            };
+        });
+    };
+
+    const handleSaveAll = () => {
+        console.log(radioButtonStates);
+    };
+
+    const renderChildren = () => {
+        let children = [];
+        for (let i = 0; i < numChildren; i++) {
+            children.push(
+                <Child key={i} id={i} handleSave={handleSave} />
+            );
+        }
+        return children;
+    };
+
+    return (
+        <div>
+            <div className="button-container">
+                <button onClick={handleAddChild}>Add</button>
+                <button onClick={handleSaveAll}>Save</button>
+            </div>
+            <div className="radio-group-container">{renderChildren()}</div>
+        </div>
+    );
 }
